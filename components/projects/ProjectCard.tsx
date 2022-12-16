030c0855cea4f16072f310b6e1c0ac4116a3e6a8
Chew Yi Xin <chewyixin1999@gmail.com>
2022-12-16 11:26:40 +0800

    project card below medium breakpoint

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -25,15 +25,41 @@ const ProjectCard = ({ projectObject }: ProjectCardProps) => {
   const { src, imageClassName, alt, width, height } = imgProps;
 
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl color-shadow rounded-xl group">
-      <Image
-        width={width}
-        height={height}
-        src={src}
-        alt={alt}
-        className={`w-full ease-in-out duration-200 group-hover:opacity-20 rounded-xl ${imageClassName}`}
-      />
-      <div className="hidden group-hover:block absolute p-3 ">
+    <div className="shadow-xl color-shadow rounded-xl">
+      <div className="relative flex items-center justify-center h-auto w-full md:shadow-xl color-shadow rounded-xl group">
+        <Image
+          width={width}
+          height={height}
+          src={src}
+          alt={alt}
+          className={`w-full ease-in-out duration-200 md:group-hover:opacity-20 rounded-t-xl md:rounded-xl ${imageClassName}`}
+        />
+        {/* Hover overlay */}
+        <div className="hidden md:group-hover:block absolute p-3 ">
+          <p className="project-title font-bold">{title}</p>
+          <p className="project-subtitle">{subtitle}</p>
+          <p className="text-xs pb-2">{dateString}</p>
+          <hr />
+          <p className="project-body-text">{bodyText}</p>
+          {/* Skills section */}
+          <div className="project-body-text">
+            {renderSkills(skills, skills.length !== 0)}
+          </div>
+          <div className="flex justify-between text-xs items-center">
+            {/* add option for target to not be _blank */}
+            {/* Github */}
+            <div className="project-link project-body-text">
+              {renderGithub(githubUrl, githubUrl.length !== 0)}
+            </div>
+            {/* Project url */}
+            <div className="project-link project-body-text">
+              {renderProjectPath(projectPath, projectPath.length !== 0)}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Card description before md breakpoint */}
+      <div className="px-3 py-2 md:hidden">
         <p className="project-title font-bold">{title}</p>
         <p className="project-subtitle">{subtitle}</p>
         <p className="text-xs pb-2">{dateString}</p>
