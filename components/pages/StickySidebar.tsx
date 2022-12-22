d145acda270ba0d7d61607cfe31f3569166f2734
Chew Yi Xin <chewyixin1999@gmail.com>
2022-12-22 11:35:56 +0800

    changes to project sidebar

--- components/pages/StickySidebar.tsx
+++ components/pages/StickySidebar.tsx
@@ -21,9 +21,19 @@ const StickySidebar = ({ links }: StickySidebarProps) => {
       <div className="sticky top-[100px] text-right">
         <div className="py-1 title">Sections</div>
         <ul className="opacity-50">
+          <li
+            onClick={handleClick}
+            className="py-1 hover:underline hover:color-text-accent"
+          >
+            <Link href={""}>Back to top</Link>
+          </li>
           {Object.keys(links).map((link, index) => {
             return (
-              <li onClick={handleClick} key={index} className="py-1">
+              <li
+                onClick={handleClick}
+                key={index}
+                className="py-1 hover:underline hover:color-text-accent"
+              >
                 <Link
                   href={link}
                   className={
